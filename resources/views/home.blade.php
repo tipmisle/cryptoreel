602a6093734f3bc16c215866d1f9a9d022d2bb3b
Miha Vidakovič <miha@vidakovic.si>
2017-09-25 01:29:22 +0200

    added sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,13 +33,11 @@
                 </li>
             </ul>
         </div>
-        <div class="col col-lg-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="col col-lg-4 sidebar">
+            <h2>My portfolio statistics</h2>
+            <div class="statistics">
+                <h3>$6323.63</h3>
+            </div>
         </div>
     </div>
 </div>
